df189b9f0df1488981c76ad8097a6b6c090ee5d4
Christopher De Leon <18326238+CDeLeon94@users.noreply.github.com>
2020-06-05 09:51:09 -0500

    Showtime table for via

--- src/components/GroupPages/Content/Viatorum/Showtimes.js
+++ src/components/GroupPages/Content/Viatorum/Showtimes.js
@@ -1,68 +1,54 @@
 import React from 'react';
-import { Grid, Button, Card, CardContent, CardMedia, CardActions} from '@material-ui/core';
-import viaLogo from '../../../../img/Viatorum/Logo.png'
+import { Grid, Button, Paper} from '@material-ui/core';
+import viaLogo from '../../../../img/BohemianQuartet/Logo.JPG'
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 650,
+    margin: 0,
+  },
+});
+
+
+function createData(venue, date, city, ticketprice) {
+  return { venue, date, city, ticketprice };
+}
+
+const rows = [
+  createData('Tobin Auditorium', '1/1/2020', 'San Antonio, Texas', 100),
+  createData('Austin City Limits Auditorium', '1/2/2020', 'Austin, Texas', 100),
+];
 
 export default function App() {
-    return (
-        <Grid item container direction="row" height="2vh" className="GroupPage_Content_Row" xs={4} spacing={2}>
-          <Grid item xs={12}>
-            <h3 color="white">Events</h3>
-          </Grid>
-          <Grid item className="showtimes" >
-            <Card variant="outlined">
-                <CardMedia>
-                <img src={viaLogo} width="100%" alt="Viatorum Logo"/>
-                </CardMedia>
-                <CardContent>
-                    <h3>January 1st, 2020</h3>
-                </CardContent>
-                <CardActions>
-                    <Button size="small">Get Tickets</Button>
-                </CardActions>
-            </Card>
-          </Grid>
-
-          <Grid item className="showtimes" >
-            <Card variant="outlined">
-                <CardMedia>
-                <img src={viaLogo} width="100%" alt="Viatorum Logo"/>
-                </CardMedia>
-                <CardContent>
-                    <h3>January 2nd, 2020</h3>
-                </CardContent>
-                <CardActions>
-                    <Button size="small">Get Tickets</Button>
-                </CardActions>
-            </Card>
-          </Grid>
-
-          <Grid item className="showtimes" >
-            <Card variant="outlined">
-                <CardMedia>
-                <img src={viaLogo} width="100%" alt="Viatorum Logo"/>
-                </CardMedia>
-                <CardContent>
-                    <h3>January 3rd, 2020</h3>
-                </CardContent>
-                <CardActions>
-                    <Button size="small">Get Tickets</Button>
-                </CardActions>
-            </Card>
-          </Grid>
-
-          <Grid item className="showtimes">
-            <Card variant="outlined">
-                <CardMedia>
-                <img src={viaLogo} width="100%" alt="Viatorum Logo"/>
-                </CardMedia>
-                <CardContent>
-                    <h3>January 4th, 2020</h3>
-                </CardContent>
-                <CardActions>
-                    <Button size="small">Get Tickets</Button>
-                </CardActions>
-            </Card>
-          </Grid>
+  const classes = useStyles();
+  return (
+        <Grid container
+        style={{backgroundColor: ''}}
+        direction="column"
+        className="GroupPage_Content_Row"
+        xs={9}
+        >
+          <TableContainer component={Paper}>
+            <Table className={classes.table}  aria-label="simple table">
+              <TableBody>
+                {rows.map((row) => (
+                  <TableRow key={row.name}>
+                    <TableCell align="right">{row.date}</TableCell>
+                    <TableCell component="th" scope="row"><b>{row.venue}</b></TableCell>
+                    <TableCell align="right">{row.city}</TableCell>
+                    <TableCell align="right"><Button variant="contained" color="Primary">Get Tickets (${row.ticketprice}+)</Button></TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
         </Grid>
     );
-}
+}  
