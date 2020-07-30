250179d45ce96badc76b11613e0fb53039c7fa3d
Christopher De leon <christopher@Christophers-MacBook-Pro.local>
2020-07-30 01:50:47 -0500

    Updates

---
+++ precache-manifest.2454b3abffbd190c0a6cf6e471a591a5.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56445d5f5f0e2a1d935d83a3942fd5d4",
+    "url": "/RenardMusicGroup/index.html"
+  },
+  {
+    "revision": "e04a84256c65a3e1aa50",
+    "url": "/RenardMusicGroup/static/css/main.73757935.chunk.css"
+  },
+  {
+    "revision": "7e80300fb6fd2fb0b094",
+    "url": "/RenardMusicGroup/static/js/2.08e1ab64.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/RenardMusicGroup/static/js/2.08e1ab64.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e04a84256c65a3e1aa50",
+    "url": "/RenardMusicGroup/static/js/main.a7f3b05d.chunk.js"
+  },
+  {
+    "revision": "dca7ebc0f60ba8e5bc9b",
+    "url": "/RenardMusicGroup/static/js/runtime-main.68695e6d.js"
+  },
+  {
+    "revision": "3baf9b00370b58beca63cb8ebeea64fe",
+    "url": "/RenardMusicGroup/static/media/1a_Transparent.3baf9b00.png"
+  },
+  {
+    "revision": "d8b848c29797f27823f8326ad2cbafd8",
+    "url": "/RenardMusicGroup/static/media/2a_Transparent.d8b848c2.png"
+  },
+  {
+    "revision": "5622551fa02d45109975039f495c80f9",
+    "url": "/RenardMusicGroup/static/media/3a_Transparent.5622551f.png"
+  },
+  {
+    "revision": "4a8c9f1274d2fe2eb857ab358fc4ff53",
+    "url": "/RenardMusicGroup/static/media/4a_Transparent.4a8c9f12.png"
+  },
+  {
+    "revision": "8b90b463093c1c98e49002b09dd9f85a",
+    "url": "/RenardMusicGroup/static/media/5a_Transparent.8b90b463.png"
+  },
+  {
+    "revision": "76c22cdb67ac63886da36f3239ef556a",
+    "url": "/RenardMusicGroup/static/media/BQ-1.76c22cdb.jpg"
+  },
+  {
+    "revision": "59583265180fc92f253265ee3ea7cff3",
+    "url": "/RenardMusicGroup/static/media/BQ-2.59583265.jpg"
+  },
+  {
+    "revision": "954378211ba48c102d8536af0e59ceb0",
+    "url": "/RenardMusicGroup/static/media/BQ-3.95437821.jpg"
+  },
+  {
+    "revision": "7b427570a3550aedcb3465a3b661862e",
+    "url": "/RenardMusicGroup/static/media/BQ-4.7b427570.jpg"
+  },
+  {
+    "revision": "fc17341fcf68716e5055736c8a32127a",
+    "url": "/RenardMusicGroup/static/media/BQ-5.fc17341f.jpg"
+  },
+  {
+    "revision": "56565249af5eb0f0cc7855bacde69d4d",
+    "url": "/RenardMusicGroup/static/media/BQ-6.56565249.jpg"
+  },
+  {
+    "revision": "64b7c49c523c7cb84b552b989b69b765",
+    "url": "/RenardMusicGroup/static/media/Logo.64b7c49c.JPG"
+  },
+  {
+    "revision": "c3c623219f8bcde62b21befaa5fd7eea",
+    "url": "/RenardMusicGroup/static/media/Logo.c3c62321.png"
+  },
+  {
+    "revision": "7255c81b0c10e067ca2667ef9c957a9f",
+    "url": "/RenardMusicGroup/static/media/RMG_Logo_Horizontal_White.7255c81b.png"
+  },
+  {
+    "revision": "d68d982f97004628f637e4cc82cbabd2",
+    "url": "/RenardMusicGroup/static/media/RenardMerch.d68d982f.png"
+  },
+  {
+    "revision": "2f8b0114a1ac205361932a8a76e8b825",
+    "url": "/RenardMusicGroup/static/media/Viatorum_Map.2f8b0114.jpg"
+  },
+  {
+    "revision": "2d63335f0d028584ef48efc46bad67bf",
+    "url": "/RenardMusicGroup/static/media/container_x2.2d63335f.png"
+  },
+  {
+    "revision": "f8fe8bc1f559b14383fe65b2935cd014",
+    "url": "/RenardMusicGroup/static/media/container_x2.f8fe8bc1.png"
+  }
+]);
